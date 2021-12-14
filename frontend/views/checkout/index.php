770ed74fca99ef290f5250779ab00cc7cf4fdc5e
agapofff <agapofff@gmail.com>
2021-12-14 06:58:41 +0300

    fix

--- frontend/views/checkout/index.php
+++ frontend/views/checkout/index.php
@@ -19,7 +19,7 @@
 
 <div class="container-fluid mb-7 mt-1_5 px-lg-2 px-xl-3 px-xxl-5">
     <div class="row justify-content-center">
-        <div class="col col-sm-10 col-md-8 col-lg-6 pr-1_5">
+        <div class="col col-sm-10 col-md-8 col-lg-6 pr-lg-1_5">
 			<?= OrderForm::widget();?>
         </div>
 		<div class="col-lg-6 d-none d-lg-block pl-1_5">
