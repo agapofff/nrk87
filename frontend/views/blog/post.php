40cec00151aa149e090848189447f70ea1041a4e
Dmitriy Agapofff <agapofff@gmail.com>
2022-01-28 19:50:00 +0300

    blog fix

--- frontend/views/blog/post.php
+++ frontend/views/blog/post.php
@@ -16,7 +16,7 @@ $this->title = json_decode($post->name)->{Yii::$app->language};
                     <?= json_decode($post->name)->{Yii::$app->language} ?>
                 </h1>
             </div>
-            <div id="blog-post-content" class="col-12">
+            <div id="blog-post-content" class="col-sm-10 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
                 <?= str_replace('<img ', '<img class="img-fluid mx-auto" loading="lazy" ', json_decode($post->text)->{Yii::$app->language}) ?>
             </div>
         </div>
