e65be297f8c3d3d797c87f61bbe0e71dac9c03e2
Dmitriy Agapofff <agapofff@gmail.com>
2022-05-17 14:02:20 +0300

    images zoom

--- frontend/views/product/index.php
+++ frontend/views/product/index.php
@@ -41,7 +41,7 @@ $sizes = json_decode($model->sizes)->{Yii::$app->language};
                     $cachedImageMax = '/images/cache/Products/Product' . $image->itemId . '/' . $image->urlAlias . '_x2000.jpg';
             ?>
                     <div class="col-12 mt-1 overflow-hidden">
-                        <!-- <a href="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImageMax) ? $cachedImageMax : $image->getUrl('x2000') ?>" data-fancybox="gallery1"> -->
+                        <a href="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImageMax) ? $cachedImageMax : $image->getUrl('x2000') ?>" data-fancybox="gallery1">
                             <img <?php if ($key == 0) {?> itemprop="image" <?php } ?> src="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImageMin) ? $cachedImageMin : $image->getUrl('x1500') ?>" class="d-block w-100" alt="<?= $image->alt ? $image->alt : $product_name ?>" loading="lazy">
                         <!-- </a> -->
                     </div>
@@ -62,9 +62,9 @@ $sizes = json_decode($model->sizes)->{Yii::$app->language};
                         $cachedImageMin = '/images/cache/Products/Product' . $image->itemId . '/' . $image->urlAlias . '_x1000.jpg';
                         $cachedImageMax = '/images/cache/Products/Product' . $image->itemId . '/' . $image->urlAlias . '_x1500.jpg';
                 ?>
-                        <!-- <a href="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImageMax) ? $cachedImageMax : $image->getUrl('x1500') ?>" data-fancybox="gallery2" data-width="100" data-height="100"> -->
+                        <a href="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImageMax) ? $cachedImageMax : $image->getUrl('x1500') ?>" data-fancybox="gallery2" data-width="100" data-height="100">
                             <img src="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImageMin) ? $cachedImageMin : $image->getUrl('x1000') ?>" class="img-fluid rounded" alt="<?= $image->alt ? $image->alt : $product_name ?>" loading="lazy">
-                        <!-- </a> -->
+                        </a>
                 <?php
                     }
                 ?>
