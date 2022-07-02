8c65b5c6e0798dd23668872cd45b7d77572c625b
Dmitriy Agapofff <agapofff@gmail.com>
2022-07-02 05:58:51 +0300

    Update index.php

--- frontend/views/product/index.php
+++ frontend/views/product/index.php
@@ -76,7 +76,7 @@ $sizes = json_decode($model->sizes)->{Yii::$app->language};
             
                 <div class="col-12 mb-2">
                 
-                    <div class="owl-carousel owl-theme owl-dots">
+                    <div class="owl-carousel owl-theme owl-dots owl-autoheight">
                     
                 <?php
                     foreach ($images as $key => $image) {
