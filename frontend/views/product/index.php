75e3e3b2f4670c85585370d93d02bd9ab85755f9
Dmitriy Agapofff <agapofff@gmail.com>
2022-07-02 21:45:36 +0300

    Update index.php

--- frontend/views/product/index.php
+++ frontend/views/product/index.php
@@ -58,7 +58,7 @@ $sizes = json_decode($model->sizes)->{Yii::$app->language};
                 foreach ($images as $key => $image) {
             ?>
                     <div class="col-12 mt-1 overflow-hidden">
-                        <a href="<?= $productImages[$key]['max'] ?>" data-fancybox="gallery" data-image="<?= $key ?>">
+                        <a href="<?= $productImages[$key]['max'] ?>" data-fancybox="gallery1">
                             <img src="data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs=" data-src="<?= $productImages[$key]['min'] ?>" class="d-block w-100 lazyload" alt="<?= $image->alt ? $image->alt : $product_name ?>" <?php if ($key == 0) {?>itemprop="image"<?php } ?>>
                         </a>
                     </div>
