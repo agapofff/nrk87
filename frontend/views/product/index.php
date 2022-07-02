f525d3154ca4f03ea6d6b47fb3bb00a3afb982f6
Dmitriy Agapofff <agapofff@gmail.com>
2022-07-02 03:00:46 +0300

    Update index.php

--- frontend/views/product/index.php
+++ frontend/views/product/index.php
@@ -34,7 +34,7 @@ foreach ($images as $key => $image) {
         'max' => file_exists(Yii::getAlias('@frontend') . '/web' . $imageMax) ? $imageMax : $image->getUrl('x' . $imgMax),
     ];
 }
-
+print_r(ArrayHelper::getColumn($productImages, 'max'));
 $this->registerJs("preloadImages(" . json_encode(ArrayHelper::getColumn($productImages, 'max')) . ");", View::POS_READY);
 
 $product_name = json_decode($model->name)->{Yii::$app->language};
